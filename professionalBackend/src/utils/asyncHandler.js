d59e93d1cec8b17635f98ee22a19c5c61ed01ba7
Chesta31832 <chesta3121@gmail.com>
2025-06-19 09:34:28 +0530

    four

---
+++ professionalBackend/src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// const asyncHandler = (fn) => (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     }catch (error) {
+//         res.status(error.status || 500).json({
+//             success: false,
+//             message: error.message || 'Internal Server Error',
+//         });
+// };
+//     next(error);
+// }
+const asyncHandler =(requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => {
+                res.status(error.status || 500).json({
+                    success: false,
+                    message: error.message || 'Internal Server Error',
+                });
+                next(error);
+            });
+    };
+}
+
+export default asyncHandler;
