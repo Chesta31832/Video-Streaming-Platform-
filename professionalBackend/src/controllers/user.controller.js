82c43cc80f69689f5ced35935424a9bbe735da5f
Chesta31832 <chesta3121@gmail.com>
2025-06-21 09:07:17 +0530

    four

--- professionalBackend/src/controllers/user.controller.js
+++ professionalBackend/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import apiError from '../utils/apiError.js';
 import {User} from '../models/user.model.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import {apiResponse} from '../utils/apiResponse.js';
+import jwt from 'jsonwebtoken';
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
@@ -150,4 +151,47 @@ const logoutUser = asyncHandler(async (req, res) => {
         );
 });
 
-export { registerUser, loginUser, logoutUser }
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if (!incomingRefreshToken) {
+        throw new apiError(401, 'Refresh token is required');
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+        )
+    
+        const user = await User.findById(decodedToken?._id)
+    
+        if(!user){
+            throw new apiError(401, 'User not found');
+        }
+    
+        if(user?.refreshToken !== incomingRefreshToken) {
+            throw new apiError(401, 'Invalid refresh token');
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true,
+        };
+    
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+    
+        return res
+            .cookie('accessToken', accessToken, options)
+            .cookie('refreshToken', newRefreshToken, options)
+            .status(200)
+            .json(
+                new apiResponse(200, {accessToken, refreshToken: newRefreshToken}, 'Access token refreshed successfully')
+            );
+    } catch (error) {
+        throw new apiError(401, error?.message || 'Unauthorized access, invalid refresh token');
+    }
+
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
