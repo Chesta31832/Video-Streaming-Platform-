3ed9283d113fdbd7a5f73a776ab187769ed9b73b
Chesta31832 <chesta3121@gmail.com>
2025-06-22 11:30:07 +0530

    four

--- professionalBackend/src/routes/user.route.js
+++ professionalBackend/src/routes/user.route.js
@@ -1,7 +1,7 @@
 import {Router} from 'express';
 import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentUserPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from '../controllers/user.controller.js';
-import {upload} from '../middlewares/multer.middleware.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
+import {upload} from '../middlewares/multer.meddleware.js';
+import { verifyJWT } from '../middlewares/auth.meddleware.js';
 
 
 
